97c393fe54bba9b8589bafa750d83624efba1ab3
Florentina165 <florentinaibishi@hotmail.com>
2023-12-16 23:23:17 +0100

    added orderitems table binding

--- webapp/controller/SalesOrderDetail.controller.js
+++ webapp/controller/SalesOrderDetail.controller.js
@@ -14,12 +14,20 @@ sap.ui.define([
                 oRouter.getRoute("detail").attachPatternMatched(this._onSalesorderMatched, this);
             },
             _onSalesorderMatched: function (oEvent) {
-                let sSalesorderID = oEvent.getParameter("arguments").salesorder || "0";
+                var sSalesorderID = oEvent.getParameter("arguments").salesorder || "0";
+                var sSalesorderPath = `/sales_orderSet('${sSalesorderID}')`;
+            
                 this.getView().bindElement({
-                    path: `/sales_orderSet('${sSalesorderID}')`,
-                    // model: ""
+                    path: sSalesorderPath,
+                });
+            
+                var oTable = this.getView().byId("salesorderitemTable");
+                oTable.bindItems({
+                    path: sSalesorderPath + "/sales_order_itemSet",
+                    template: oTable.getBindingInfo("items").template
                 });
             },
+                   
 
             onExit: function () {
                 this.oRouter.getRoute("list").detachPatternMatched(this._onSalesorderMatched, this);
