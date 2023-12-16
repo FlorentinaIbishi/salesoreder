5a69ac8d8058462169ae6d5673a8b4b845c35647
Florentina165 <florentinaibishi@hotmail.com>
2023-12-16 20:53:11 +0100

    initial commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("ap.salesoreder.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
